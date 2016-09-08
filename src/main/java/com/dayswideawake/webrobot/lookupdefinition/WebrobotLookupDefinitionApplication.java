91de7d380accb39f6f437978ba5202027cae15a7
Zoltan Nyaray <Zoltan_Nyaray@epam.com>
2016-09-08 12:32:56 +0200

    yolo

--- src/main/java/com/dayswideawake/webrobot/lookupdefinition/WebrobotLookupDefinitionApplication.java
+++ src/main/java/com/dayswideawake/webrobot/lookupdefinition/WebrobotLookupDefinitionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Application {
+public class WebrobotLookupDefinitionApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(WebrobotLookupDefinitionApplication.class, args);
 	}
 }
