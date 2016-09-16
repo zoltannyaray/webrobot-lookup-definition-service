bfa9db7ec2afe85571a2fbf21240f988860783f7
Zoltan Nyaray <Zoltan_Nyaray@epam.com>
2016-09-16 17:04:24 +0200

    yolo

--- src/main/java/com/dayswideawake/webrobot/lookupdefinition/messaging/Channels.java
+++ src/main/java/com/dayswideawake/webrobot/lookupdefinition/messaging/Channels.java
@@ -6,8 +6,8 @@ import org.springframework.messaging.MessageChannel;
 public interface Channels {
 
 	String CHANNEL_OUTPUT_NEW_LOOKUP_DEFINITIONS = "new-lookup-definitions";
-	
+
 	@Output(CHANNEL_OUTPUT_NEW_LOOKUP_DEFINITIONS)
 	MessageChannel newLookupDefinitions();
-	
+
 }
